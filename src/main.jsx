0ebd65870a449660d06c3accac17f276ccc1d540
faiz-oussama <faizouss123@gmail.com>
2025-04-02 11:38:13 +0000

    final touches

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
+import { Analytics } from "@vercel/analytics/react"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 import DarkModeProvider from './components/DarkModeProvider.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <DarkModeProvider>
+      <Analytics />
       <App />
     </DarkModeProvider>
   </StrictMode>,
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  future: {
+    optimizeUniversalDefaults: false
+  },
   content: {
     relative: true,
     files: [
